cdb0e2f68d5a31266d252a2fd9f7c5a52dd34cb1
HarryRadit <harrydxm@gmail.com>
2024-10-13 19:23:00 +0700

    buk

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-myname = "Nig"
+title = "my ever book list"
+books={
+    {
+        'id':1,
+        'title': "Harry potter",
+        'author': "rio",
+        'year': 2001,
+        'description': "some description about the book harry potter"
+    },
+    {
+        'id': 2,
+        'title': "doraemon",
+        'author': "hayakawa",
+        'year': 2011,
+        'description': "nobita meet doraemon"
+    },
+}
 @app.route('/')
 def hello_world():
-    return 'Hello, ger! from ' + myname
+   return render_template('block_list.html', title=title)
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-print("test")
